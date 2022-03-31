0402e5d79a293322a4cb0b2b1b062c631fe5b666
Chidi Gbujie <chidi.gbujie@centrifugegroup.com>
2022-03-31 23:59:03 +0100

    Created Service Layer

---
+++ springboot-residentapp/src/main/java/com/chidiudoapp/residentapp/services/ResidentService.java
@@ -0,0 +1,4 @@
+package com.chidiudoapp.residentapp.services;
+
+public interface ResidentService {
+}
---
+++ springboot-residentapp/src/main/java/com/chidiudoapp/residentapp/services/ResidentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.chidiudoapp.residentapp.services;
+
+import com.chidiudoapp.residentapp.model.Resident;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResidentServiceImpl implements  ResidentService{
+}
