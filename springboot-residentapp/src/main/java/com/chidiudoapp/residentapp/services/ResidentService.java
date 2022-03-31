51eb0c89edac4462e1622c86d66a64bac21b740a
Chidi Gbujie <gbujiechidi@yahoo.com>
2022-04-01 00:47:56 +0100

    Refactored Save Residents

--- springboot-residentapp/src/main/java/com/chidiudoapp/residentapp/services/ResidentService.java
+++ springboot-residentapp/src/main/java/com/chidiudoapp/residentapp/services/ResidentService.java
@@ -1,5 +1,8 @@
 package com.chidiudoapp.residentapp.services;
 
+import com.chidiudoapp.residentapp.model.Resident;
+
 public interface ResidentService {
 
+    public Resident addResident(Resident resident);
 }
--- springboot-residentapp/src/main/java/com/chidiudoapp/residentapp/services/ResidentServiceImpl.java
+++ springboot-residentapp/src/main/java/com/chidiudoapp/residentapp/services/ResidentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.chidiudoapp.residentapp.services;
 
+import com.chidiudoapp.residentapp.model.Resident;
 import com.chidiudoapp.residentapp.repository.ResidentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ public class ResidentServiceImpl implements ResidentService{
 
     @Autowired
     private ResidentRepository residentRepository;
+
+    @Override
+    public Resident addResident(Resident resident) {
+        return residentRepository.save(resident);
+    }
 }
